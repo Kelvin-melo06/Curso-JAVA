820f253ef0062649098e22e5e3744364a4ffc15d
Kelvin Melo <kmelo7882@gmail.com>
2025-09-01 14:59:32 -0300

    exercicios java

--- src/aula_java/Main.java
+++ src/aula_java/Main.java
@@ -1,7 +1,12 @@
 package aula_java;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
+    public static void main(String[] args){
+        int resultado = somar(5,7);
+    }
+    public static int somar(int a ,int b){
+        int resultadoConta = a+b;
+        System.out.println("O resultado entre " + a + " + " + b + " é de " + resultadoConta);
+        return resultadoConta;
     }
 }
